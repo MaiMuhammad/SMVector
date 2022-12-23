55308c858e4fe62b926725d74fc5c2242043af05
MAIMOHAMED8 <102184232+MAIMOHAMED8@users.noreply.github.com>
2022-12-23 16:03:42 +0200

    Create SMVector.cpp

---
+++ SMVector.cpp
@@ -0,0 +1,24 @@
+#include "SMVector.h"
+int main(){
+    SMVector <int> vec(8);
+    vec.push_back( 5);
+    vec.push_back( 2);
+    vec.push_back( 8);
+    vec.push_back( 10);
+    vec.push_back( 6);
+    vec.push_back( 9);
+
+    SMVector <int> vec2(10);
+    vec2.push_back( 10);
+    vec2.push_back( 2);
+    vec2.push_back( 8);
+    vec2.push_back( 5);
+    vec2.push_back( 6);
+    vec2.push_back( 9);
+
+//    vec.pop_back();
+//    vec.erase(vec.begin(), vec.end());
+//    vec.erase(vec.begin());
+//    vec.clear();
+//    cout<<(vec==vec2);
+}
