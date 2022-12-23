024d789dc701eb777d085c8afc3f8b9dd6123bf6
MAIMOHAMED8 <102184232+MAIMOHAMED8@users.noreply.github.com>
2022-12-23 16:12:14 +0200

    Update SMVector.h

--- SMVector.h
+++ SMVector.h
@@ -32,5 +32,65 @@ public:
     friend ostream& operator << (ostream& out, const SMVector <T>&);
 };
 
+template <class T>
+SMVector<T> ::SMVector(int cap) {
+    capacity = cap;
+    size = 0;
+    vector = new T[capacity];
+}
+
+template <class T>
+SMVector<T>::SMVector(T* array , int n) {
+    size = n;
+    vector = new T[size];
+    for(size_t i=0; i<size ; i++){
+        vector[i]=array[i];
+    }
+}
+
+template <class T>
+SMVector<T>::SMVector (const SMVector& vec){
+    size=vec.size;
+    capacity=vec.capacity;
+    vector=new T[capacity];
+    for(size_t i=0; i<vec.size; i++){
+        vector[i]=vec.vector[i];
+    }
+}
+
+template <class T>
+SMVector<T>::~SMVector(){
+    delete[]vector;
+}
+
+//copy assignment
+template <class T>
+SMVector<T> & SMVector<T> :: operator = (const SMVector& vec) {
+    if(this!= &vec){
+        delete [] vector;
+        size = vec.size;
+        capacity = vec.capacity;
+        vector = new T[capacity];
+        for(size_t i=0; i< vec.size; i++){
+            vector[i]=vec.vector[i];
+        }
+        return *this;
+    }
+    else{
+        cout << "Cannot delete self." << endl;
+    }
+}
+
+//Move assignment
+template <class T>
+SMVector<T> & SMVector<T> :: operator = (const SMVector&& vec) {
+    this->size=size;
+    this->capacity=capacity;
+    this->vector=vector;
+    vec.size=0;
+    vec.capacity=0;
+    vec.vector= nullptr;
+    return *this;
+}
 
 
